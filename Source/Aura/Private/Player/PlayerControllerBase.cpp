c51d37f53275f998d8eedc3ced3ad50fc9b9bace
hanapig <1152389162@qq.com>
2025-04-20 22:58:10 +0800

    创建动画蓝图和模板，创建增强输入

---
+++ Source/Aura/Private/Player/PlayerControllerBase.cpp
@@ -0,0 +1,48 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Player/PlayerControllerBase.h"
+
+#include "InputState.h"
+
+APlayerControllerBase::APlayerControllerBase()
+{
+	bReplicates = true; 
+}
+
+void APlayerControllerBase::BeginPlay()
+{
+	Super::BeginPlay();
+
+	//增强输入子系统
+	check(CharacterContext);
+	UEnhancedInputLocalPlayerSubsystem*Subsystem=ULocalPlayer::GetSubsystem<UEnhancedInputLocalPlayerSubsystem>(GetLocalPlayer());
+	check(Subsystem);
+	Subsystem->AddMappingContext(CharacterContext,0);
+
+	//鼠标
+	bShowMouseCursor = true; 
+	DefaultMouseCursor = EMouseCursor::Default;
+	FInputModeGameAndUI InputModeData;
+	InputModeData.SetLockMouseToViewportBehavior(EMouseLockMode::DoNotLock);
+	InputModeData.SetHideCursorDuringCapture(false);
+	SetInputMode(InputModeData);
+}
+
+void APlayerControllerBase::SetupInputComponent()
+{
+	Super::SetupInputComponent();
+	
+	//获取组件绑定移动函数
+	UEnhancedInputComponent*EnhancedInputComponent=Cast<UEnhancedInputComponent>(InputComponent);
+	EnhancedInputComponent->BindAction(MoveAction,ETriggerEvent::Triggered,this,&APlayerControllerBase::Move);
+}
+
+void APlayerControllerBase::Move(const struct FInputActionValue& InputActionValue)
+{
+	const FVector2d InputAxisVector=InputActionValue.Get<FVector2D>();
+	const FRotator InputRotation=GetControlRotation();
+	const FRotator InputAxis(0.f,InputRotation.Yaw,0.f);
+	
+	
+}
---
+++ Source/Aura/Public/Player/PlayerControllerBase.h
@@ -0,0 +1,39 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/PlayerController.h"
+#include "Components/InputComponent.h"       
+#include "EnhancedInputComponent.h"          
+#include "EnhancedInputSubsystems.h"      
+#include "InputAction.h"                 
+#include "InputMappingContext.h"            
+#include "PlayerControllerBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class AURA_API APlayerControllerBase : public APlayerController
+{
+	GENERATED_BODY()
+
+public:
+
+	APlayerControllerBase();
+	
+protected:
+	virtual void BeginPlay() override;
+	virtual void SetupInputComponent() override; 
+
+private:
+	UPROPERTY(EditAnywhere, Category="Input")
+	TObjectPtr<UInputMappingContext> CharacterContext;
+
+	UPROPERTY(EditAnywhere, Category="Input")
+	TObjectPtr<UInputAction> MoveAction;
+
+	void Move(const struct FInputActionValue& InputActionValue);
+
+};
